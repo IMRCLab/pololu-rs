6cf6f82ab80b90d4e8a818e1fd731d950abf4fd7
Wolfgang Hoenig <hoenig@tu-berlin.de>
2025-03-12 16:54:20 +0100

    motors working

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # pololu3pi2040
+
+##
+
+* Press "B" + Reset
+* `cargo run --release` will flash and run the firmware
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@
 #![no_std]
 #![no_main]
 
+use embedded_hal::pwm::SetDutyCycle;
 // The macro for our start-up function
 use rp_pico::entry;
 
@@ -77,16 +78,57 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Set the LED to be an output
+    // See https://github.com/pololu/pololu-3pi-2040-robot/blob/master/c/pololu_3pi_2040_robot/yellow_led.c
+    // See https://github.com/rp-rs/rp-hal-boards/blob/main/boards/rp-pico/examples/pico_blinky.rs
+
+    // // Set the LED to be an output
     let mut led_pin = pins.led.into_push_pull_output();
 
-    // Blink the LED at 1 Hz
+    // // Blink the LED at 1 Hz
+    // loop {
+    //     led_pin.set_high().unwrap();
+    //     delay.delay_ms(500);
+    //     led_pin.set_low().unwrap();
+    //     delay.delay_ms(500);
+    // }
+
+    // See https://github.com/pololu/pololu-3pi-2040-robot/blob/master/c/pololu_3pi_2040_robot/motors.c
+    // See https://github.com/rp-rs/rp-hal-boards/blob/main/boards/rp-pico/examples/pico_pwm_servo.rs
+    // 
+
+    // Init PWMs
+    let mut pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+
+    // Configure PWM7
+    let pwm = &mut pwm_slices.pwm7;
+    pwm.set_ph_correct();
+    pwm.set_div_int(1); // 20.8 KhZ
+    pwm.set_div_frac(0);
+    pwm.set_top(/*MOTORS_MAX_SPEED - 1 */ 6000 - 1);
+    pwm.enable();
+
+    let mut direction_right = pins.gpio10.into_push_pull_output_in_state(hal::gpio::PinState::Low); // direction right
+    let mut direction_left = pins.gpio11.into_push_pull_output_in_state(hal::gpio::PinState::Low); // direction left
+
+    let channel_right = &mut pwm.channel_a;
+    channel_right.output_to(pins.gpio14);
+
+    let channel_left = &mut pwm.channel_b;
+    channel_left.output_to(pins.gpio15);
+
     loop {
+        channel_right.set_duty_cycle(3000).unwrap();
+        direction_right.set_high().unwrap();
+
+        channel_left.set_duty_cycle(1000).unwrap();
+        direction_left.set_low().unwrap();
+
         led_pin.set_high().unwrap();
         delay.delay_ms(500);
         led_pin.set_low().unwrap();
         delay.delay_ms(500);
     }
+
 }
 
 // End of file
